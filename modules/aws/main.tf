838b4c9fe0aa61ae1db6a846adcaa88e9b027d76
xris nelson <chris.nelson@campspot.com>
2022-11-23 14:18:23 -0700

    add providers

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -16,41 +16,41 @@ terraform {
 }
 
 provider "kubernetes" {
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+  host                   = var.cluster-endpoint
+  cluster_ca_certificate = base64decode(var.cluster-certificate-authority-data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", var.cluster-name]
   }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+    host                   = var.cluster-endpoint
+    cluster_ca_certificate = base64decode(var.cluster-certificate-authority-data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       # This requires the awscli to be installed locally where Terraform is executed
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", var.cluster-name]
     }
   }
 }
 
 provider "kubectl" {
   apply_retry_count      = 5
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate  = base64decode(var.cluster_certificate_authority_data)
+  host                   = var.cluster-endpoint
+  cluster_ca_certificate  = base64decode(var.cluster-certificate-authority-data)
   load_config_file         = false
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", var.cluster-name]
   }
 }
--- modules/aws/variables-aws.tf
+++ modules/aws/variables-aws.tf
@@ -1,14 +1,9 @@
-variable "cluster_name" {
-  description = "(Required) The name of the EKS cluster where Karpenter is going to be installed"
-  type        = string
-}
-
-variable "cluster_endpoint" {
+variable "cluster-endpoint" {
   description = "(Required) The endpoint of the EKS cluster where Karpenter is going to be installed"
   type        = string
 }
 
-variable "cluster_certificate_authority_data" {
+variable "cluster-certificate-authority-data" {
   description = "(Required) The CA data for the cluster where Karpenter is going to be installed"
   type        = string
 }
